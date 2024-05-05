68a19847aef7c3916a85250b7fda0ab5b9281971
ChillBroh <isharamadusanka410@gmail.com>
2024-05-06 00:11:23 +0530

    url hard coded

--- config/db.js
+++ config/db.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 const connectDb = async () => {
   try {
-    const con = await mongoose.connect(process.env.MONGO_URI);
+    const con = await mongoose.connect(
+      "mongodb+srv://isharamadusanka0714:235689@cluster0.hz5h9vs.mongodb.net/NASA"
+    );
     console.log(`MongoDB connected : ${con.connection.host} 😎`);
   } catch (error) {
     console.log(error);
